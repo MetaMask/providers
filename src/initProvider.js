a3665182e4eeadf66fb3eb2394445b2107851751
Erik Marks <rekmarks@protonmail.com>
2020-04-20 11:17:32 -0700

    proxyHandler -> preventPropertyDeletion

--- src/initProvider.js
+++ src/initProvider.js
@@ -1,30 +1,22 @@
 const MetamaskInpageProvider = require('./MetamaskInpageProvider')
 
-// Work around for web3@1.0 deleting the bound `sendAsync` but not the unbound
-// `sendAsync` method on the prototype, causing `this` reference issues
-const defaultProxyHandler = {
-  // straight up lie that we deleted the property so that it doesnt
-  // throw an error in strict mode
-  deleteProperty: () => true,
-}
-
 /**
    * Initializes a MetamaskInpageProvider and (optionally) sets it on window.ethereum.
    *
    * @param {Object} opts - An options bag.
    * @param {Object} opts.connectionStream - A Node.js stream.
    * @param {number} opts.maxEventListeners - The maximum number of event listeners.
+   * @param {boolean} opts.preventPropertyDeletion - Whether to wrap the provider in a proxy that prevents property deletion.
    * @param {boolean} opts.shouldSendMetadata - Whether the provider should send page metadata.
-   * @param {Object} opts.proxyHandler - A proxy handler object. The provider is proxied if present.
-   * @param {boolean} opts.shouldSet - Whether the provider should be set as window.ethereum
+   * @param {boolean} opts.shouldSetOnWindow - Whether the provider should be set as window.ethereum
    * @returns {MetamaskInpageProvider | Proxy} The initialized provider (whether set or not).
    */
 function initProvider ({
   connectionStream,
-  shouldSendMetadata = true,
   maxEventListeners = 100,
-  proxyHandler = defaultProxyHandler,
-  shouldSet = true,
+  preventPropertyDeletion = true,
+  shouldSendMetadata = true,
+  shouldSetOnWindow = true,
 }) {
 
   if (!connectionStream) {
@@ -35,11 +27,15 @@ function initProvider ({
     connectionStream, { shouldSendMetadata, maxEventListeners },
   )
 
-  if (proxyHandler) {
-    provider = new Proxy(provider, proxyHandler)
+  if (preventPropertyDeletion) {
+    // Workaround for web3@1.0 deleting the bound `sendAsync` but not the unbound
+    // `sendAsync` method on the prototype, causing `this` reference issues
+    provider = new Proxy(provider, {
+      deleteProperty: () => true,
+    })
   }
 
-  if (shouldSet) {
+  if (shouldSetOnWindow) {
     setGlobalProvider(provider)
   }
 
