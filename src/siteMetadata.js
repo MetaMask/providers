6dc275a758b319186b683cb473f301024d1c6ece
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-07-21 17:47:51 -0230

    Update siteMetadata.js

--- src/siteMetadata.js
+++ src/siteMetadata.js
@@ -64,7 +64,7 @@ function getSiteName (window) {
 
 /**
  * Extracts an icon for the site from the DOM
- * @returns {string} a icon URL
+ * @returns {string|null} a icon URL
  */
 async function getSiteIcon (window) {
   const { document } = window
