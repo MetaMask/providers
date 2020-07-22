2c7aad07ad7b8835d0ac15de5fb38cc676e31e1c
Erik Marks <rekmarks@protonmail.com>
2020-07-22 08:33:15 -0700

    finish constructor tests

--- test/MetamaskInpageProvider.test.js
+++ test/MetamaskInpageProvider.test.js
@@ -11,8 +11,63 @@ describe('MetamaskInpageProvider', () => {
       expect(() => new MetamaskInpageProvider(new DuplexStream())).not.toThrow()
     })
 
-    it('throws if no stream is provided', () => {
-      expect(() => new MetamaskInpageProvider()).toThrow(messages.errors.invalidDuplexStream())
+    it('succeeds if stream and valid options are provided', () => {
+      const stream = new DuplexStream()
+
+      expect(
+        () => new MetamaskInpageProvider(stream, {
+          maxEventListeners: 10,
+        }),
+      ).not.toThrow()
+
+      expect(
+        () => new MetamaskInpageProvider(stream, {
+          shouldSendMetadata: false,
+        }),
+      ).not.toThrow()
+
+      expect(
+        () => new MetamaskInpageProvider(stream, {
+          maxEventListeners: 10,
+          shouldSendMetadata: false,
+        }),
+      ).not.toThrow()
+    })
+
+    it('throws if no or invalid stream is provided', () => {
+      expect(
+        () => new MetamaskInpageProvider(),
+      ).toThrow(messages.errors.invalidDuplexStream())
+
+      expect(
+        () => new MetamaskInpageProvider('foo'),
+      ).toThrow(messages.errors.invalidDuplexStream())
+
+      expect(
+        () => new MetamaskInpageProvider({}),
+      ).toThrow(messages.errors.invalidDuplexStream())
+    })
+
+    it('throws if bad options are provided', () => {
+      const stream = new DuplexStream()
+
+      expect(
+        () => new MetamaskInpageProvider(stream, null),
+      ).toThrow('Cannot destructure property `maxEventListeners` of \'undefined\' or \'null\'')
+
+      expect(
+        () => new MetamaskInpageProvider(stream, {
+          maxEventListeners: 10,
+          shouldSendMetadata: 'foo',
+        }),
+      ).toThrow(messages.errors.invalidOptions(10, 'foo'))
+
+      expect(
+        () => new MetamaskInpageProvider(stream, {
+          maxEventListeners: 'foo',
+          shouldSendMetadata: true,
+        }),
+      ).toThrow(messages.errors.invalidOptions('foo', true))
     })
   })
 })
