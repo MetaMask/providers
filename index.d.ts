8eb3316cc307b6d740070d7569230cf2d1c792b4
Erik Marks <rekmarks@protonmail.com>
2020-08-19 09:33:49 -0700

    add full docstrings to declarations file

--- index.d.ts
+++ index.d.ts
@@ -25,53 +25,88 @@ export interface MetamaskInpageProviderOptions {
 export class MetamaskInpageProvider extends EventEmitter {
 
   /**
-   * @param connectionStream A Node.js duplex stream
-   * @param options An options bag
+   * @param connectionStream - A Node.js duplex stream.
+   * @param options - An options bag.
    */
   constructor (connectionStream: Duplex, options?: MetamaskInpageProviderOptions);
 
+  /**
+   * Returns whether the provider can process RPC requests.
+   */
   isConnected (): boolean;
 
   /**
-   * Submits an RPC request to MetaMask for the given method, with the given params.
+   * Submits an RPC request for the given method, with the given params.
    * Resolves with the result of the method call, or rejects on error.
    */
   request (args: RequestArguments): Promise<unknown>;
 
+  /**
+   * Submits an RPC request per the given JSON-RPC request object.
+   */
   sendAsync (
-    payload: JsonRpcPayload,
+    payload: JsonRpcRequest,
     callback: (error: Error | null, result?: JsonRpcResponse) => void,
   ): void;
 
+  /**
+   * Submits an RPC request for the given method, with the given params.
+   * @deprecated Use {@link request} instead.
+   */
   send (method: string, params?: unknown[]): Promise<JsonRpcResponse>;
 
+  /**
+   * Submits an RPC request per the given JSON-RPC request object.
+   * @deprecated Use {@link request} instead.
+   */
   send (
-    payload: JsonRpcPayload,
+    payload: JsonRpcRequest,
     callback: (error: Error | null, result?: JsonRpcResponse) => void,
   ): void;
 
-  send (payload: SendSyncJsonRpcPayload): JsonRpcResponse;
+  /**
+   * Accepts a JSON-RPC request object, and synchronously returns the cached result
+   * for the given method. Only supports 4 specific methods.
+   * @deprecated Use {@link request} instead.
+   */
+  send (payload: SendSyncJsonRpcRequest): JsonRpcResponse;
 
+  /**
+   * Indicating that this provider is a MetaMask provider.
+   */
   readonly isMetaMask: true;
 
+  /**
+   * The user's currently selected Ethereum address.
+   * If null, MetaMask is either locked or the user has not permitted any
+   * addresses to be viewed.
+   */
   readonly selectedAddress: string | null;
 
+  /**
+   * The network ID of the currently connected Ethereum chain.
+   * @deprecated Use {@link chainId} instead.
+   */
   readonly networkVersion: string | null;
 
+  /**
+   * The chain ID of the currently connected Ethereum chain.
+   * See [chainId.network]{@link https://chainid.network} for more information.
+   */
   readonly chainId: string | undefined;
 }
 
 /**
- * Initializes a MetamaskInpageProvider and (optionally) sets it on window.ethereum.
+ * Initializes a MetamaskInpageProvider and (optionally) assigns it as window.ethereum.
  * @returns The initialized provider (whether set or not).
  */
 export function initProvider (
   opts: Pick<MetamaskInpageProviderOptions, 'maxEventListeners' | 'shouldSendMetadata'> & {
 
-    /** A Node.js duplex stream */
+    /** A Node.js duplex stream. */
     connectionStream: Duplex;
 
-    /** Whether the provider should be set as window.ethereum */
+    /** Whether the provider should be set as window.ethereum. */
     shouldSetOnWindow?: boolean;
   }
 ): MetamaskInpageProvider;
@@ -85,31 +120,52 @@ export function initProvider (
 export function setGlobalProvider (providerInstance: MetamaskInpageProvider): void;
 
 export interface RequestArguments {
+
+  /** The RPC method to request. */
   method: string;
+
+  /** The params of the RPC method, if any. */
   params?: unknown[];
 }
 
-/**
- * The jsonrpc and id properties will be handled if not provided.
- */
-export interface JsonRpcPayload {
+export interface JsonRpcRequest {
+
+  /** The RPC method to request. */
   method: string;
+
+  /** The params of the RPC method, if any. */
   params?: unknown[];
+
+  /** For spec compliance; handled if not provided. */
   id?: string | number;
+
+  /** For spec compliance; handled if not provided. */
   jsonrpc?: '2.0';
 }
 
-export interface JsonRpcResponse {
-  jsonrpc: '2.0';
-  id: string | number;
-  result?: unknown;
-  error?: {
+export interface SendSyncJsonRpcRequest extends JsonRpcRequest {
+  method: 'eth_accounts' | 'eth_coinbase' | 'eth_uninstallFilter' | 'net_version';
+}
+
+interface JsonRpcResponseBase {
+
+  /** Equal to the corresponding JSON-RPC request object. */
+  id?: string | number;
+
+  /** Equal to the corresponding JSON-RPC request. */
+  jsonrpc?: '2.0';
+}
+
+export interface JsonRpcErrorResponse extends JsonRpcResponseBase {
+  error: {
     code: number;
     message: string;
     data?: unknown;
   };
 }
 
-interface SendSyncJsonRpcPayload extends JsonRpcPayload {
-  method: 'eth_accounts' | 'eth_coinbase' | 'eth_uninstallFilter' | 'net_version';
+export interface JsonRpcSuccessResponse extends JsonRpcResponseBase {
+  result: unknown;
 }
+
+export type JsonRpcResponse = JsonRpcSuccessResponse | JsonRpcErrorResponse;
