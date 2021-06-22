68d03e8b1692ee7ea87aabdebad132d570f9d82a
Erik Marks <rekmarks@protonmail.com>
2021-06-22 10:29:06 -0700

    Update lint dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -41,19 +41,21 @@ export function createErrorMiddleware(
 }
 
 // resolve response.result or response, reject errors
-export const getRpcPromiseCallback = (
-  resolve: (value?: any) => void,
-  reject: (error?: Error) => void,
-  unwrapResult = true,
-) => (error: Error, response: PendingJsonRpcResponse<unknown>): void => {
-  if (error || response.error) {
-    reject(error || response.error);
-  } else {
-    !unwrapResult || Array.isArray(response)
-      ? resolve(response)
-      : resolve(response.result);
-  }
-};
+export const getRpcPromiseCallback =
+  (
+    resolve: (value?: any) => void,
+    reject: (error?: Error) => void,
+    unwrapResult = true,
+  ) =>
+  (error: Error, response: PendingJsonRpcResponse<unknown>): void => {
+    if (error || response.error) {
+      reject(error || response.error);
+    } else {
+      !unwrapResult || Array.isArray(response)
+        ? resolve(response)
+        : resolve(response.result);
+    }
+  };
 
 /**
  * Logs a stream disconnection error. Emits an 'error' if given an
