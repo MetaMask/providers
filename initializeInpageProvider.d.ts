8d85f6be94cdcc33c49a6f5c71e80233fbb5b1ae
Mark Stacey <markjstacey@gmail.com>
2024-11-27 16:40:58 -0330

    Use alternative strategy that works better for ESM builds
    Add a new top-level export map entry for `initializeProvider` that maps
    correctly onto CJS and ESM builds, and add a fallback JavaScript file
    at the root of the repository for build systems that don't support
    export maps.
    
    This gives the best of both worlds: build systems that support exports
    will behave correctly, and those that don't will get the CJS fallback.

---
+++ initializeInpageProvider.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+
+// Re-exported for compatibility with build tools that don't support the
+// `exports` field in package.json
+export * from './dist/initializeInpageProvider.cjs';
