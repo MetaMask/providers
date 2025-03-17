bc4f9c9f2e13da9d2b58867593efcc56dc08689b
Jiexi Luan <jiexiluan@gmail.com>
2025-03-17 11:40:50 -0700

    add comment about createExternalExtensionProvider

--- src/extension-provider/createExternalExtensionProvider.ts
+++ src/extension-provider/createExternalExtensionProvider.ts
@@ -15,6 +15,7 @@ export type ExtensionType = 'stable' | 'flask' | 'beta' | string;
 
 /**
  * Creates an external extension provider for the given extension type or ID.
+ * This is intended for use by 3rd party extensions.
  *
  * @param typeOrId - The extension type or ID.
  * @returns The external extension provider.
